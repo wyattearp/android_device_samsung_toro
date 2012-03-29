1503b8bc287a8310fd09e4a8d898fdf1050468ae
Liquid Smooth <liquid0624@gmail.com>
2012-03-29 14:03:13 -0400

    Fix 404 Market Build Fingerprint

--- liquid.mk
+++ liquid.mk
@@ -23,11 +23,10 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=mysid \
     TARGET_BUILD_TYPE=user \
     BUILD_VERSION_TAGS=release-keys \
-    BUILD_DISPLAY_ID="Liquid ICS v1.2" \
-    PRIVATE_BUILD_DESC="mysid-user 4.0.4 IMM76D 257829 release-keys" \
-    BUILD_FINGERPRINT=google/mysid/toro:4.0.4/IMM76D/257829:user/release-keys
+    BUILD_DISPLAY_ID="Liquid ICS v1.25" \
+    PRIVATE_BUILD_DESC="mysid-user 4.0.4 IMM30B 257829 release-keys" \
+    BUILD_FINGERPRINT=google/mysid/toro:4.0.4/IMM30B/257829:user/release-keys
 
 # media
 PRODUCT_COPY_FILES += \
     vendor/liquid/prebuilt/common/media/xhdpi/bootanimation.zip:system/media/bootanimation.zip
-
