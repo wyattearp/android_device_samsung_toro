96ef888187c22d2028a5c720b01999ff313835ce
Liquid Smooth <liquid0624@gmail.com>
2012-04-16 00:55:37 -0400

    Fix Build Fingerprint/Increase Version Number

--- liquid.mk
+++ liquid.mk
@@ -19,13 +19,13 @@ PRODUCT_PROPERTY_OVERRIDES += ro.modversion=liquid.toro.$(shell date +%m%d%y).$(
 PRODUCT_BUILD_PROP_OVERRIDES += \
     USER=liquid \
     BUILD_ID=IMM76D \
-    BUILD_NUMBER=257829 \
+    BUILD_NUMBER=235179 \
     PRODUCT_NAME=mysid \
     TARGET_BUILD_TYPE=user \
     BUILD_VERSION_TAGS=release-keys \
-    BUILD_DISPLAY_ID="Liquid ICS v1.25" \
-    PRIVATE_BUILD_DESC="mysid-user 4.0.4 IMM30B 257829 release-keys" \
-    BUILD_FINGERPRINT=google/mysid/toro:4.0.4/IMM30B/257829:user/release-keys
+    BUILD_DISPLAY_ID="Liquid ICS v1.3" \
+    PRIVATE_BUILD_DESC="mysid-user 4.0.2 ICL53F 235179 release-keys" \
+    BUILD_FINGERPRINT=google/mysid/toro:4.0.2/ICL53F/235179:user/release-keys
 
 # media
 PRODUCT_COPY_FILES += \
